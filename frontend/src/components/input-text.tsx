fc73602aea4ae00478ea8abc22999bcde42ea1f4
laurindolucas <caiolucax2024@gmail.com>
2026-01-04 17:14:31 -0300

    feat: adicionando componentes de inupt

---
+++ frontend/src/components/input-text.tsx
@@ -0,0 +1,47 @@
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import { textVariants } from "./text";
+
+export const inputTextVariants = cva(
+  `
+   rounded-[var(--radius)]
+    border border-[hsl(var(--border))]
+    bg-[hsl(var(--card))] text-[hsl(var(--card-foreground))]
+    shadow-[var(--shadow-card)]
+  `,
+  {
+    variants: {
+      size: {
+        md: "pb-2 px-2",
+      },
+      disabled: {
+        true: "pointer-events-none",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+      disabled: false,
+    },
+  }
+);
+
+interface InputTextProps
+  extends VariantProps<typeof inputTextVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {}
+
+export default function InputText({
+  size,
+  disabled,
+  className,
+  ...props
+}: InputTextProps) {
+  return (
+    <input
+      className={cx(
+        inputTextVariants({ size, disabled }),
+        textVariants(),
+        className
+      )}
+      {...props}
+    />
+  );
+}
